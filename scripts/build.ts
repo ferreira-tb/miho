6a70da330cee169d14825cf35814e67c69491410
Andrew Ferreira <andrew.shien2@gmail.com>
2023-11-23 17:39:07 -0300

    chore: move `execa` to dependencies

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,13 +3,13 @@ import * as fs from 'node:fs/promises';
 import process from 'node:process';
 import { fileURLToPath } from 'node:url';
 import { existsSync as exists } from 'node:fs';
-import { execa } from 'execa';
+import { $ } from 'execa';
 
 try {
   const dirname = path.dirname(fileURLToPath(import.meta.url));
   const dist = path.resolve(dirname, '../dist');
   if (exists(dist)) await fs.rm(dist, { recursive: true });
-  await execa('run-s', ['rollup', 'minify'], { stdio: 'inherit' });
+  await $({ stdio: 'inherit' })`run-s rollup minify`;
 } catch (err) {
   console.error(err);
   process.exit(1);
