4fa35c0ac89ce56027ce2fe1e95cf0a05f80fcea
Andrew Ferreira <andrew.shien2@gmail.com>
2023-11-22 21:42:54 -0300

    chore: update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export * from './config';
-export * from './miho';
-export * from './utils/enum';
-export type * from './package';
-export type * from './types';
+  export * from './config';
+   export * from './miho';
+           export * from './utils/enum';
+  export type * from './package';
+    export type * from './types';
