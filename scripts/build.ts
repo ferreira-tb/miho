e29788b654cb157eb44af857b2433e2b4c540d42
Andrew Ferreira <andrew.shien2@gmail.com>
2023-11-29 18:20:01 -0300

    chore: bump version

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,13 +3,13 @@ import process from 'node:process';
 import * as fs from 'node:fs/promises';
 import { fileURLToPath } from 'node:url';
 import { existsSync as exists } from 'node:fs';
-import { $ } from 'execa';
+import { execa } from 'execa';
 
 try {
   const dirname = path.dirname(fileURLToPath(import.meta.url));
   const dist = path.resolve(dirname, '../dist');
   if (exists(dist)) await fs.rm(dist, { recursive: true });
-  await $({ stdio: 'inherit' })`run-s rollup minify`;
+  await execa('pnpm', ['rollup'], { stdio: 'inherit' });
 } catch (err) {
   console.error(err);
   process.exit(1);
