90a44a94467157449af09953659ecc6732e58c0e
Andrew Ferreira <andrew.shien2@gmail.com>
2023-12-24 22:39:41 -0300

    chore: convert some basic flags

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct MihoCli {
+  /// Commit all modififed files, not only the packages.
+  #[arg(short = 'a', long, default_value_t = true)]
+  all: bool,
+
+  /// Determines whether Miho should ask for consent.
+  #[arg(long, default_value_t = true)]
+  ask: bool,
+
+  /// Commit the modified packages.
+  #[arg(short = 'c', long, default_value = "chore: bump version")]
+  commit: String,
+
+  /// Skip all jobs.
+  #[arg(long)]
+  dry_run: bool,
+}
+
+fn main() -> Result<()> {
+  let cli = MihoCli::parse();
+  println!("{:?}", cli);
+
+
+
+  Ok(())
 }
