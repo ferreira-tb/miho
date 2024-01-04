7a87b2ea6d0830625b4edfea4292a5fa612a7c8c
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-04 00:28:17 -0300

    refactor: use `if..else`

--- src/macros.rs
+++ src/macros.rs
@@ -1,13 +1,13 @@
 /// Wrap [std::process::Command], executing `cmd` as the program if the current OS is Windows.
 ///
 /// This is only useful in some very specific cases.
-/// Prefer the standard library version.
 #[macro_export]
 macro_rules! win_cmd {
   ($program:literal) => {{
-    let mut cmd = match std::env::consts::OS {
-      "windows" => std::process::Command::new("cmd"),
-      _ => std::process::Command::new($program),
+    let mut cmd = if std::env::consts::OS == "windows" {
+      std::process::Command::new("cmd")
+    } else {
+      std::process::Command::new($program)
     };
 
     if std::env::consts::OS == "windows" {
