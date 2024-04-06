7ccd2e9fe9885c9dd12af380a13cdfeb554906d8
Andrew Ferreira <andrew.shien2@gmail.com>
2024-04-05 23:05:21 -0300

    chore: bump version

--- miho/src/prelude.rs
+++ miho/src/prelude.rs
@@ -1,14 +1,14 @@
 pub use anyhow::{anyhow, bail, Result};
+pub use colored::Colorize;
 pub use globset::{Glob, GlobSet, GlobSetBuilder};
+pub use hashbrown::{HashMap, HashSet};
 pub use itertools::Itertools;
 pub use semver::{BuildMetadata, Comparator, Op, Prerelease, Version, VersionReq};
 pub use serde::Deserialize;
 pub use std::cmp::Ordering;
-pub use std::collections::{HashMap, HashSet};
 pub use std::path::{Path, PathBuf};
 pub use std::sync::{Arc, Mutex, OnceLock};
 pub use std::{env, fmt, fs, mem};
 pub use strum::IntoEnumIterator;
 pub use tokio::process::Command;
 pub use tokio::task::JoinSet;
-pub use colored::Colorize;
